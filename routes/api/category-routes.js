b982033d31cb6c96437bf07a7e9dee62fe5d3e31
NateB <nathanbrengman@gmail.com>
2022-08-22 18:24:03 -0500

    added code to routes/api

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,37 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll(
+    {
+      attributes: ['id', 'category_name'],
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      ]
+    }
+  ).then(dbCategoryData => {
+    res.json(dbCategoryData)
+  }).catch(err => {
+    console.log(err)
+    res.status(400).json(err)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'catergory_name'],
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }]
+  })
 });
 
 router.post('/', (req, res) => {
